59793df48dd6b1679f8b10f712ee41a532ff9b87
Hélio Júnior <heliorpjunior116@gmail.com>
2023-12-30 12:32:44 -0300

    feat: instanciação e importação da entidade Task e migração Createtasks

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,12 +1,14 @@
+import Task from '@modules/tasks/infra/typeorm/entities/Task';
 import { DataSource } from 'typeorm';
+import { CreateTasks1703949339910 } from './migrations/1703949339910-CreateTasks';
 
 export const dataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
   port: 5432,
   username: 'postgres',
-  password: '5432',
+  password: '43710',
   database: 'tasks',
-  entities: [],
-  migrations: [],
+  entities: [Task],
+  migrations: [CreateTasks1703949339910],
 });
